c4419ea6a7a76815e199e17dc6a909f2fce5c1fd
jasaga2401 <jasaga@protonmail.com>
2023-11-19 22:26:54 +0000

    Add project files.

---
+++ db_connection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        //  SQL Server connection string
+        string connectionString = "Data Source=Amaze\\SQLEXPRESS;Initial Catalog=things_to_do;Integrated Security=True";
+
+        // Create a new SqlConnection using the connection string
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                // Open the database connection
+                connection.Open();
+                Console.WriteLine("Connected to the database.");
+
+                // Your database operations go here...
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                // Ensure the connection is closed, whether an exception occurs or not
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    Console.WriteLine("Connection closed.");
+                }
+            }
+        }
+    }
+}
+
